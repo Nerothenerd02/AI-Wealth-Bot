bcb25132207258e891d0b326de099cc31c5fc0ea
Navien07 <navientharan10@gmail.com>
2025-04-20 16:15:07 +0800

    Fixing Errors and File Directory

--- basic_app/utils/get_stock_info.py
+++
@@ -1,8 +0,0 @@
-import urllib.request, json
-
-def getStockInfo(var):
-    var = var.replace(' ','')
-    url = "https://finance.yahoo.com/_finance_doubledown/api/resource/searchassist;searchTerm={}?device=console&returnMeta=true".format(var)
-    response = urllib.request.urlopen(url)
-    data = json.loads(response.read())
-    return data['data']['items']
